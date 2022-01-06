7afd2167955837b80c492d99d2c1e20330797285
cch0807 <https://github.com/cch0807>
2022-01-06 11:04:25 +0900

    new

---
+++ westarbucks/my_settings.py
@@ -0,0 +1,13 @@
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'starbucks',
+        'USER': 'root',
+        'PASSWORD': '19950807',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-bbo+i1=jgm%pl+*x9brn#y#&aw-klwornb5=c@m84iah9wz02i' #settings.py에 있는 

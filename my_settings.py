8f0ba7bdec6a06e950d9f69a286e5efb0862c4ab
cch0807 <https://github.com/cch0807>
2022-01-08 11:26:33 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+
+SECRET_KEY = 'django-insecure-7=en7gu$5qul$2pn2@!b^%$6rvfb4s!!=^l++xs93-)q1$g$nm'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud1',
+        'USER': 'root',
+        'PASSWORD': '19950807',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
